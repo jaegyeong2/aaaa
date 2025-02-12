0b63bf16694d290bf6950f08d4055c4f9593b802
jaegyeong2 <163654540+jaegyeong2@users.noreply.github.com>
2025-02-13 02:10:16 +0900

    Update main.py

--- back/main.py
+++ back/main.py
@@ -16,6 +16,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
