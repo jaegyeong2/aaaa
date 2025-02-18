0316e1921c57ddf229c19c8174b67c5a3bc83608
jaegyeong2 <163654540+jaegyeong2@users.noreply.github.com>
2025-02-18 17:16:25 +0900

    Update database.py

--- back/database.py
+++ back/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:worud12345@database-2.c940uuimi242.ap-northeast-2.rds.amazonaws.com:5432/postgres"
 
 engine = create_engine(
