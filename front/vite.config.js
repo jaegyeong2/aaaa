76fd254dad12e24d83fc1ca95ee92a7b642ff771
jaegyeong2 <fbworud123456@naver.com>
2025-03-01 00:25:16 +0900

    das

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    crypto: 'crypto-browserify',  // crypto 폴리필 설정
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',  // crypto-browserify 모듈로 대체
+    },
+  },
 })
